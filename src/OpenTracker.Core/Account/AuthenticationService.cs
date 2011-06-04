2c8ea882aac165db5cea796cfba67aa91f2dd3cd
Søren Rasmussen <brokzftw@gmail.com>
2011-06-04 14:44:13 +0200

    login now also returns the id, so we can set the formsauthentication custom userdata.

--- src/OpenTracker.Core/Account/AuthenticationService.cs
+++ src/OpenTracker.Core/Account/AuthenticationService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Web;
 using System.Web.Security;
 
@@ -9,7 +10,7 @@ namespace OpenTracker.Core.Account
     /// </summary>
     public interface IFormsAuthenticationService
     {
-        void SignIn(string userName, bool createPersistentCookie);
+        void SignIn(string userName, bool createPersistentCookie, int userid);
         void SignOut();
     }
 
@@ -23,24 +24,27 @@ namespace OpenTracker.Core.Account
         /// </summary>
         /// <param name="userName"></param>
         /// <param name="createPersistentCookie"></param>
-        public void SignIn(string userName, bool createPersistentCookie)
+        /// <param name="userId"></param>
+        public void SignIn(string userName, bool createPersistentCookie, int userId)
         {
             if (String.IsNullOrEmpty(userName)) 
                 throw new ArgumentException("Value cannot be null or empty.", "userName");
+            if (userId <= 0)
+                throw new ArgumentException("Value cannot be null or empty.", "userId");
 
-            // FormsAuthentication.SetAuthCookie(userName, createPersistentCookie); // default MVC3 login
+            var userData = string.Format(" ;{0}", userId);
             var authTicket = new FormsAuthenticationTicket(
-                1,                                  // version
-                userName,                           // user name
-                DateTime.Now,                       // created
-                DateTime.Now.AddMinutes(15),        // expires
-                createPersistentCookie,             // persistent?
-                userData: "Moderator;Administrator" // can be used to store roles etc.
+                1,                              // version
+                userName,                       // user name
+                DateTime.Now,                   // created
+                DateTime.Now.AddMinutes(15),    // expires
+                createPersistentCookie,         // persistent?
+                userData                        // can be used to store roles etc.
             );
 
             var encryptedTicket = FormsAuthentication.Encrypt(authTicket);
-
             var authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+
             HttpContext.Current.Response.Cookies.Add(authCookie);
         }
 
---
+++ src/OpenTracker/Controllers/Forum/ForumController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace OpenTracker.Controllers.Forum
+{
+    public class ForumController : Controller
+    {
+        //
+        // GET: /Forum/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
