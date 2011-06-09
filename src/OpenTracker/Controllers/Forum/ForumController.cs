c23d3d1feb4867c46315b21b6065a8b66004879b
Søren Rasmussen <brokzftw@gmail.com>
2011-06-09 20:49:38 +0200

    added forum preperation.

--- src/OpenTracker/Controllers/Forum/ForumController.cs
+++ src/OpenTracker/Controllers/Forum/ForumController.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System.Data;
+using System.Data.Objects;
+using System.Linq;
+using System.Web.Mvc;
+using OpenTracker.Core;
+using OpenTracker.Models.Forum;
 
 namespace OpenTracker.Controllers.Forum
 {
@@ -6,11 +11,63 @@ namespace OpenTracker.Controllers.Forum
     {
         //
         // GET: /Forum/
-
+        //
         public ActionResult Index()
         {
+            /*
+            using (var context = new OpenTrackerDbContext())
+            {
+                var forum = (from c in context.forum_category
+                             join f in context.forum on c.id equals f.categoryid
+                             join t in context.forum_topic on f.id equals t.forumid
+                             join tc in context.forum_topic on f.id equals tc.forumid into tcount
+                             join p in context.forum_posts on t.id equals p.topicid
+                             join pc in context.forum_posts on t.id equals pc.topicid into pcount
+                             join u in context.users on p.userid equals u.id
+                             orderby p.added descending
+                             select new ForumIndexModel
+                                        {
+                                            CategoryId = c.id,
+                                            CategoryTitle = c.title,
+
+                                            ForumId = f.id,
+                                            ForumTitle = f.title,
+                                            ForumDescription = f.description,
+                                            TopicId = t.id,
+                                            TopicTitle = t.title,
+
+                                            PostId = p.id,
+                                            PostAdded = p.added,
+
+                                            Username = u.username,
+
+                                            TopicCount = tcount.Count(),
+                                            PostCount = pcount.Count()
+                                        }).ToList();
+
+
+
+
+                // var ggqjler = forum.ToGeneratedSql();
+                return View(forum);
+            }
+            */
             return View();
         }
-
     }
+
+    public static class Test
+    {
+
+        public static string ToGeneratedSql(this IQueryable query)
+        {
+            var _objectQuery = query as ObjectQuery;
+
+            if (_objectQuery == null)
+                return null;
+
+            return _objectQuery.ToTraceString();
+        }
+    }
+
 }
---
+++ src/OpenTracker/Models/Forum/ForumModel.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenTracker.Core;
+
+namespace OpenTracker.Models.Forum
+{
+    public class ForumIndexModel
+    {
+        public long CategoryId { get; set; }
+        public string CategoryTitle { get; set; }
+
+        public long ForumId { get; set; }
+        public string ForumTitle { get; set; }
+        public string ForumDescription { get; set; }
+
+        public long TopicId { get; set; }
+        public string TopicTitle { get; set; }
+
+        public long PostId { get; set; }
+        public long PostAdded { get; set; }
+
+        public string Username { get; set; }
+
+        public int TopicCount { get; set; }
+        public int PostCount { get; set; }
+
+    }
+
+    
+    
+    public class ForumTestModel
+    {
+        static OpenTrackerDbContext context = new OpenTrackerDbContext();
+
+        public List<forum_category> Categories
+        {
+            get
+            {
+                var categoriez = (from c in context.forum_category 
+                                  select c).ToList();
+                return categoriez;
+            }
+        }
+
+        public static List<forum> Forum(int categoryid)
+        {
+                /*
+                             join f in context.forum on c.id equals f.categoryid
+                             join t in context.forum_topic on f.id equals t.forumid
+                             join tc in context.forum_topic on f.id equals tc.forumid into tcount
+                             join p in context.forum_posts on t.id equals p.topicid
+                             join pc in context.forum_posts on t.id equals pc.topicid into pcount
+                             join u in context.users on p.userid equals u.id
+                             orderby p.added descending
+
+                */
+            var _forumz = (from f in context.forum
+                            join t in context.forum_topic on f.id equals t.forumid
+                            join tc in context.forum_topic on f.id equals tc.forumid into tcount
+                            join p in context.forum_posts on t.id equals p.topicid
+                            join pc in context.forum_posts on t.id equals pc.topicid into pcount
+                            join u in context.users on p.userid equals u.id
+                            where f.id == categoryid
+                            select new
+                                       {
+                                           ForumId = f.id,
+                                           ForumTitle = f.title,
+                                           ForumDescription = f.description,
+                                           TopicId = t.id,
+                                           TopicTitle = t.title,
+
+                                           PostId = p.id,
+                                           PostAdded = p.added,
+
+                                           Username = u.username,
+
+                                           TopicCount = tcount.Count(),
+                                           PostCount = pcount.Count()
+
+                                       });
+            return _forumz as List<forum>;
+        }
+
+
+    }
+
+
+}
