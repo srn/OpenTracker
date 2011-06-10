7f0e4351f84514b317b82d83bd05d5c72eddaf1d
Søren Rasmussen <brokzftw@gmail.com>
2011-06-10 17:17:49 +0200

    added stringextension class with string abbreviation.

---
+++ src/OpenTracker.Core/Common/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OpenTracker.Core.Common
+{
+    public static class StringExtensions
+    {
+        public static string AbbreviateString(this string text, int length)
+        {
+            if (length < 0 || String.IsNullOrEmpty(length.ToString()))
+                throw new ArgumentOutOfRangeException("length", length, @"length must be > 0");
+
+            if (length == 0 || text.Length == 0)
+                return string.Empty;
+
+            if (text.Length <= length)
+                return text;
+
+            return string.Format("{0}...", text.Substring(0, length));
+        }
+
+    }
+}
