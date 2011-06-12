be3ed16e90d67f8a20e46bef60126437ac5a3605
Søren Rasmussen <brokzftw@gmail.com>
2011-06-12 19:29:41 +0200

    fixed proper ratio formatting and added a MenuHelper for active links instead of dynamic ViewBags.

---
+++ src/OpenTracker.Core/Common/Helpers/MenuHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace OpenTracker.Core.Common.Helpers
+{
+    public static class MenuHelper
+    {
+        public static MvcHtmlString MenuItem(
+            this HtmlHelper htmlHelper,
+            string text,
+            string action,
+            string controller
+        )
+        {
+            var li = new TagBuilder("li");
+            var routeData = htmlHelper.ViewContext.RouteData;
+            var currentAction = routeData.GetRequiredString("action");
+            var currentController = routeData.GetRequiredString("controller");
+            if (string.Equals(currentAction, action, StringComparison.OrdinalIgnoreCase) &&
+                string.Equals(currentController, controller, StringComparison.OrdinalIgnoreCase))
+            {
+                li.AddCssClass("current");
+            }
+            li.InnerHtml = htmlHelper.ActionLink(text, action, controller).ToHtmlString();
+            return MvcHtmlString.Create(li.ToString());
+        }
+
+    }
+
+}
